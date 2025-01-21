49b357a283b2809de0df880041560c72da92d850
ppabam <tom@diginori.com>
2025-01-21 12:14:36 +0900

    add Options

--- src/eda_bam/cli.py
+++ src/eda_bam/cli.py
@@ -2,7 +2,10 @@ from president_speech.db.parquet_interpreter import get_parquet_full_path
 import pandas as pd
 import typer
 
-def group_by_count(keyword: str):
+def group_by_count(keyword: str, asc: bool = False, rcnt: int = 12):
+    """
+    지정한 keyword로 필터링하여 대통령별로 그룹화하고, count를 기준으로 정렬하여 출력합니다.
+    """
     # TODO: ascending, 출력 rows size
     # pytest 코드를 만들어 보세요
     # import this <- 해석해보세요
@@ -10,8 +13,8 @@ def group_by_count(keyword: str):
     df = pd.read_parquet(data_path)
     fdf = df[df['speech_text'].str.contains(keyword, case=False)]
     gdf = fdf.groupby("president").size().reset_index(name="count")
-    sdf = gdf.sort_values(by='count', ascending=False).reset_index(drop=True)
-    print(sdf.to_string(index=False))
+    sdf = gdf.sort_values(by='count', ascending=asc).reset_index(drop=True)
+    print(sdf.head(rcnt).to_string(index=False))
 
 def entry_point():
     typer.run(group_by_count)
