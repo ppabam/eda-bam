2396acba0300dbebd3ebc3da8a8b9cb571c1d013
ppabam <tom@diginori.com>
2025-01-21 12:02:41 +0900

    TODO

--- src/eda_bam/cli.py
+++ src/eda_bam/cli.py
@@ -3,6 +3,9 @@ import pandas as pd
 import typer
 
 def group_by_count(keyword: str):
+    # TODO: ascending, 출력 rows size
+    # pytest 코드를 만들어 보세요
+    # import this <- 해석해보세요
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
     fdf = df[df['speech_text'].str.contains(keyword, case=False)]
