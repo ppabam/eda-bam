6cb64c28c7c479b1fd5f2889ce81b2769f13ec2c
ppabam <tom@diginori.com>
2025-01-21 15:49:02 +0900

    윤보선

--- src/eda_bam/cli.py
+++ src/eda_bam/cli.py
@@ -2,7 +2,7 @@ from president_speech.db.parquet_interpreter import get_parquet_full_path
 import pandas as pd
 import typer
 
-def group_by_count(keyword: str):
+def group_by_count(keyword: str, asc: bool=False, rcnt: int=12) -> pd.DataFrame:
     # TODO: ascending, 출력 rows size
     # pytest 코드를 만들어 보세요
     # import this <- 해석해보세요
@@ -11,8 +11,14 @@ def group_by_count(keyword: str):
     fdf = df[df['speech_text'].str.contains(keyword, case=False)]
     gdf = fdf.groupby("president").size().reset_index(name="count")
     sdf = gdf.sort_values(by='count', ascending=False).reset_index(drop=True)
-    print(sdf.to_string(index=False))
+    rdf = sdf.head(rcnt)
+    return rdf
+
+def print_group_by_count(keyword: str, asc: bool=False, rcnt: int=12):
+    df = group_by_count(keyword, asc, rcnt)
+    print(df.to_string(index=False))
+
 
 def entry_point():
-    typer.run(group_by_count)
+    typer.run(print_group_by_count)
 
