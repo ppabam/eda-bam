2fb661e380d77919c69906f94c670f9059de8999
ppabam <tom@diginori.com>
2025-01-23 15:18:10 +0900

    iat

--- tests/test_search_speech.py
+++ tests/test_search_speech.py
@@ -2,6 +2,21 @@ from eda_bam.cli import group_by_count
 import pandas as pd
 import pytest
 
+presidents_speeches = {
+    "박정희": 513,
+    "이승만": 438,
+    "노태우": 399,
+    "김대중": 305,
+    "문재인": 275,
+    "김영삼": 274,
+    "이명박": 262,
+    "전두환": 242,
+    "노무현": 230,
+    "박근혜": 111,
+    "최규하": 14,
+    "윤보선": 1
+}
+
 def test_search_exception():
     row_count = 13
     df = group_by_count(keyword="자유", asc=True, rcnt=row_count)
@@ -11,7 +26,7 @@ def test_search_exception():
     assert len(df) < row_count
 
 @pytest.mark.parametrize("is_asc, president", [(True, "윤보선"), (False, "박정희")])
-def test_sort(is_asc: bool, president: str):
+def test_정열_및_행수(is_asc: bool, president: str):
     # given
     row_count = 3
 
@@ -35,21 +50,6 @@ def test_정열_및_행수제한():
     assert isinstance(df, pd.DataFrame)
     assert df.iloc[0]["president"] == "윤보선"
     assert len(df) == row_count
-
-presidents_speeches = {
-    "박정희": 513,
-    "이승만": 438,
-    "노태우": 399,
-    "김대중": 305,
-    "문재인": 275,
-    "김영삼": 274,
-    "이명박": 262,
-    "전두환": 242,
-    "노무현": 230,
-    "박근혜": 111,
-    "최규하": 14,
-    "윤보선": 1
-}
 
 @pytest.mark.parametrize("p_name, s_count", presidents_speeches.items())
 def test_default_args_check_count_mark(p_name: str, s_count: int):
@@ -78,7 +78,7 @@ def test_all_count():
     # global dict
 
     # when
-    df = group_by_count("자유")
+    df = group_by_count("자유", True)
     
     # then
     assert isinstance(df, pd.DataFrame)
@@ -88,7 +88,19 @@ def test_all_count():
         president_row = df[df["president"] == p_name]
         assert president_row.iloc[0]["count"] == s_count
 
+def test_all_count_iat():
+    # given
+    # global dict
 
+    # when
+    df = group_by_count("자유")
+    
+    # then
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 12
+    
+    for i, (p_name, s_count) in enumerate(presidents_speeches.items()):
+        assert df.iat[i, 1] == s_count
 
 
 
